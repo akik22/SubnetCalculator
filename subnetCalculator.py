f5604e59e2dab69a2072240da2680e12a8416ec7
akik22 <akik22@gmail.com>
2017-11-16 20:39:34 +0600

    Network ID,Broadcast ID, Subnet Mask completed

--- subnetCalculator.py
+++ subnetCalculator.py
@@ -2,40 +2,54 @@
 
 given_input = input()
 
-ip,subnet_mask = given_input.split('/')
+ip,subnet_mask_number = given_input.split('/')
 
 ip = list(map(int,ip.split('.')))
-subnet_mask = int(subnet_mask)
+subnet_mask_number = int(subnet_mask_number)
 
 # IP and Subnet Mask Detached
 
-binary_ip = list(map(bin,ip))
+# binary_ip = list(map(bin,ip))
 
-mask_string = ''
+IP_in_binary = [i[2:].zfill(8) for i in list(map(bin,ip))]
 
-for i in range(32):
-    if i < subnet_mask:
-        mask_string = mask_string + '1'
-    else:
-        mask_string = mask_string + '0'
+IP_binary_string = ''.join(IP_in_binary)
 
+# print('IP string: ', IP_binary_string)
 
-mask_octate = []
+Host_ID = IP_binary_string[0:subnet_mask_number]
+# print(Host_ID)
 
+Network_bits = '0'*(32-subnet_mask_number)
+Broadcast_bits = '1'*(32-subnet_mask_number)
 
+# print(Network_bits)
+# print(Broadcast_bits)
 
-count_number = 0
+Network_address_binary_string = Host_ID+Network_bits
+Broadcast_address_binary_string = Host_ID+Broadcast_bits
 
-for i in range(4):
-    mask_octate.append(mask_string[count_number:count_number+8])
-    count_number += 8
-
-
-new_subnet_mask = []
-
-for i in mask_octate:
-    new_subnet_mask.append(int(i,2))
-
-print(new_subnet_mask)
+# print(Network_address_binary_string)
+# print(Broadcast_address_binary_string)
 
 
+def binaray_To_String(binary_string):
+    octate = []
+    count = 0
+    IP = []
+
+    for i in range(4):
+        octate.append(binary_string[count:count+8])
+        count += 8
+
+    for i in octate:
+        IP.append(str((int(i,2))))
+
+    return '.'.join(IP)
+
+
+mask_string = ('1'*subnet_mask_number) + '0'*(32-subnet_mask_number)
+
+print(binaray_To_String(Network_address_binary_string))
+print(binaray_To_String(Broadcast_address_binary_string))
+print(binaray_To_String(mask_string))
