98df6ba7c954f1e65bf7bb79a7c1c7d4e9ef21cd
akik22 <akik22@gmail.com>
2017-11-16 21:32:28 +0600

    Initial Skeleton Completed

--- subnetCalculator.py
+++ subnetCalculator.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
 
-given_input = input()
 
-ip,subnet_mask_number = given_input.split('/')
+while True:
+    try:
+        given_input = input('Please provide IP address in XXX:XXX:XXX:XXX/YY this format: ')
+        ip, subnet_mask_number = given_input.split('/')
+        ip = list(map(int, ip.split('.')))
+        subnet_mask_number = int(subnet_mask_number)
+        break
+    except:
+        print()
+        print('Please follow the format \n')
 
-ip = list(map(int,ip.split('.')))
-subnet_mask_number = int(subnet_mask_number)
-
-# IP and Subnet Mask Detached
-
-# binary_ip = list(map(bin,ip))
 
 IP_in_binary = [i[2:].zfill(8) for i in list(map(bin,ip))]
 
 IP_binary_string = ''.join(IP_in_binary)
 
-# print('IP string: ', IP_binary_string)
 
 Host_ID = IP_binary_string[0:subnet_mask_number]
-# print(Host_ID)
+Host_bits = 32-subnet_mask_number
 
-Network_bits = '0'*(32-subnet_mask_number)
-Broadcast_bits = '1'*(32-subnet_mask_number)
 
-# print(Network_bits)
-# print(Broadcast_bits)
+Network_bits = '0'*Host_bits
+Broadcast_bits = '1'*Host_bits
+First_IP_bits = '0'*(Host_bits-1) + '1'
+Last_IP_bits = '1'*(Host_bits-1) + '0'
+
 
 Network_address_binary_string = Host_ID+Network_bits
 Broadcast_address_binary_string = Host_ID+Broadcast_bits
-
-# print(Network_address_binary_string)
-# print(Broadcast_address_binary_string)
-
+First_IP_address_binary_string = Host_ID+First_IP_bits
+Last_IP_address_binary_string = Host_ID+ Last_IP_bits
 
 def binaray_To_String(binary_string):
     octate = []
@@ -48,8 +48,12 @@ def binaray_To_String(binary_string):
     return '.'.join(IP)
 
 
-mask_string = ('1'*subnet_mask_number) + '0'*(32-subnet_mask_number)
+subnet_mask_string = ('1'*subnet_mask_number) + '0'*(32-subnet_mask_number)
 
-print(binaray_To_String(Network_address_binary_string))
-print(binaray_To_String(Broadcast_address_binary_string))
-print(binaray_To_String(mask_string))
+print()
+print('Network ID: {}'.format(binaray_To_String(Network_address_binary_string)))
+print('Broadcast Address: {}'.format(binaray_To_String(Broadcast_address_binary_string)))
+print('Subnet Mask: {}'.format(binaray_To_String(subnet_mask_string)))
+print('Usable IP range: {} to {}'.format(binaray_To_String(First_IP_address_binary_string),binaray_To_String(Last_IP_address_binary_string)))
+
+
