a786b6a8fb9f0814ab7fd472ed8bb318e7f2bc8c
akik22 <akik22@gmail.com>
2017-11-16 17:06:39 +0600

    Subnet Mask calculated

--- subnetCalculator.py
+++ subnetCalculator.py
@@ -11,15 +11,31 @@ subnet_mask = int(subnet_mask)
 
 binary_ip = list(map(bin,ip))
 
-total_octate = 4
-full_octate_number = subnet_mask//8
-partial_octate_number = full_octate_number+1
-blank_octate_number = total_octate - partial_octate_number
+mask_string = ''
 
+for i in range(32):
+    if i < subnet_mask:
+        mask_string = mask_string + '1'
+    else:
+        mask_string = mask_string + '0'
+
+
+mask_octate = []
+
+
+
+count_number = 0
+
+for i in range(4):
+    mask_octate.append(mask_string[count_number:count_number+8])
+    count_number += 8
 
 
 new_subnet_mask = []
 
-for _ in range(subnet_mask):
-    new_subnet_mask.append(255)
+for i in mask_octate:
+    new_subnet_mask.append(int(i,2))
+
+print(new_subnet_mask)
+
 
